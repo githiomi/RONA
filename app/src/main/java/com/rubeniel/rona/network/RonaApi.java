ad028c554fac3a4de10139888ab6d247624117cd
Dhosio <danielgithiomi@gmail.com>
2020-06-09 01:09:33 +0300

    Created the Rona api class

---
+++ app/src/main/java/com/rubeniel/rona/network/RonaApi.java
@@ -0,0 +1,15 @@
+package com.rubeniel.rona.network;
+
+import com.rubeniel.rona.models.Country;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface RonaApi {
+
+//    Get method
+    @GET("")
+    Call<List<Country>> getCountryData();
+}
---
+++ app/src/main/java/com/rubeniel/rona/network/RonaClient.java
@@ -0,0 +1,4 @@
+package com.rubeniel.rona.network;
+
+public class RonaClient {
+}
